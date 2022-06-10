29297943f74a97309d296da3b738ef6a3efa6520
Gian Lucca <giandacoll@gmail.com>
2022-06-10 15:35:18 -0300

    css style done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { AutoCorrection } from "./components/AutoCorrection/AutoCorrection";
 function App() {
   return (
     <div >
-   <AutoCorrection />
-      
+      <AutoCorrection />
     </div>
   );
 }
--- src/components/AutoCorrection/AutoCorrection.js
+++ src/components/AutoCorrection/AutoCorrection.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import "./Style.css"
 
 export function AutoCorrection() {
-    const [word, setWord] = useState();
+    const [word, setWord] = useState(); //Variavel state
 
    
-
+    // Função handleChange tem por objetivo validar toda a lógica do código, deixando o return mais limpo e de fácil entendimento
       function handleChange(event) {
         setWord(event.target.value);
         
@@ -15,14 +16,19 @@ export function AutoCorrection() {
         const correction = {
             realy: "really",
             wierd: "weird",
+            sku: "sky",
+            waht: "What",
           };
+        // Usando o método Object.getOwnPropertyNames conseguimos acessar o vetor das propriedades do objeto, no caso a varaivel "correction"
+        const correctionArray = Object.getOwnPropertyNames(correction); 
 
-        const correctionArray = Object.getOwnPropertyNames(correction);
 
-        divider.map((params1, params2) => {
-            let index = correctionArray.indexOf(params1);
+
+         // Realizei um map na variavel divider passando duas propriedades correndo elas pelo indexOf (retornando o primeiro índice encontrado no array)
+        divider.map((props1, props2) => {
+            let index = correctionArray.indexOf(props1);
             if (index !== -1) {
-                divider[params2] = correction[correctionArray[index]];
+                divider[props2] = correction[correctionArray[index]]; // Se minha variavel index não for igual ao indexOf(-1) então minha variavel divider(obj) recebe o valor de props2, chamando minha setWord de meu use state.
                 setWord(divider.join(" ")+ " ");
             }
             return true;
@@ -37,17 +43,15 @@ export function AutoCorrection() {
     return ( 
         <div className="container">
 
-            <h1>Test React MZ</h1>
+            <h1 className="h1">Test React MZ</h1>
 
             <textarea
+            placeholder="Put your text here..."
+            className="textarea"
             name="textarea"
             type="text"
             value={word}
             onChange={handleChange} 
-
-
-
-
             />
         </div>
      );
